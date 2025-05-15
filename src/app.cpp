57e0ee5d1d2c383e7b6b1d89251cdadfdbae5ad1
Wes2000ley <atwellwea@gmail.com>
2025-05-15 04:13:48 -0600

    making it more raii

--- src/app.cpp
+++ src/app.cpp
@@ -5,7 +5,7 @@ wxIMPLEMENT_APP(App);
 
 bool App::OnInit() {
 	MainFrame* frame = new MainFrame("TrueTunnel VPN");
-	frame->SetClientSize(800, 600);
+	frame->SetClientSize(800, 1000);
 	frame->Centre();
 	frame->Show();
 
